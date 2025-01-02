ace308f6f929cdacd2753fe065eabeceb7e205e0
virtualabishek <717abishekneupane@gmail.com>
2025-01-02 17:05:02 +0545

    Fininshed Session

--- 20daysCourse/day1/app.js
+++ 20daysCourse/day1/app.js
@@ -1,2 +1,15 @@
 console.log("Hello, from App js.");
 
+/* 
+Module System:
+
+Common JS (module, module.exports, require)
+ESCRIPT (import, export, export default, export////)
+ES6 Modules
+*/
+
+function add(a, b) {
+  return a + b;
+}
+
+module.export = add;
--- 20daysCourse/day1/index.js
+++ 20daysCourse/day1/index.js
@@ -8,24 +8,44 @@
 
 // console.log(add(2, 3));
 
-global.companyName = "Abi IT Solution";
+// global.companyName = "Abi IT Solution";
 
 // console.log(companyName);
 
-global.authorName = "Abishek Neupane";
+// global.authorName = "Abishek Neupane";
 
 // console.log(authorName);
 
-function printMe() {
-  //   var abc = "abc";
-  console.log(companyName);
-  console.log(authorName);
-}
+// function printMe() {
+//   var abc = "abc";
+//   console.log(companyName);
+//   console.log(authorName);
+// }
 
 // printMe();
 
 // console.log(global);
 
-healthPatner = "Nepal Medicity";
+// healthPatner = "Nepal Medicity";
 
-console.log(healthPatner);
+// console.log(healthPatner);
+
+// console.log(process.platform);
+
+// console.log(process.env);
+
+// setInterval(() => console.log("I will execute after 2 seconds."), 2000);
+
+// console.log("Extra Global");
+
+// console.log(__dirname);
+
+// console.log(__filename);
+
+// //import export and used on JS
+
+// //require("");
+
+const add = require("./app");
+
+console.log(add(2, 3));
