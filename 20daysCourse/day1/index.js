e1ae98d0f8d5ffdb9baf288928420c733fc53fb7
virtualabishek <717abishekneupane@gmail.com>
2025-01-02 14:50:20 +0545

    Completed Evening Session

---
+++ 20daysCourse/day1/index.js
@@ -0,0 +1,31 @@
+// console.log("Hello From Node!");
+
+// console.log(2 === 2);
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(2, 3));
+
+global.companyName = "Abi IT Solution";
+
+// console.log(companyName);
+
+global.authorName = "Abishek Neupane";
+
+// console.log(authorName);
+
+function printMe() {
+  //   var abc = "abc";
+  console.log(companyName);
+  console.log(authorName);
+}
+
+// printMe();
+
+// console.log(global);
+
+healthPatner = "Nepal Medicity";
+
+console.log(healthPatner);
