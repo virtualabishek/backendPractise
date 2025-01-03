7979b06446ede74c0cefb35293af49e59fe2eddd
virtualabishek <717abishekneupane@gmail.com>
2025-01-03 20:59:58 +0545

    learning: completed jan - 3 (day -3 ) last session

---
+++ 20daysCourse/day3/index.js
@@ -0,0 +1,20 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/books") {
+    res.writeHead(200);
+    res.end("Book Page is working");
+  } else if (req.url === "/others") {
+    res.writeHead(200);
+    res.end("Others is working.");
+  } else {
+    res.writeHead(200);
+    res.end("Backend is on working mode.");
+  }
+  console.log(res);
+  // do tomorrow this using switch case.
+});
+
+server.listen(8000, () => {
+  console.log("Server started succesfully!!!!");
+});
