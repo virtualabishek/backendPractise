bf7afec5b1a8c9f10c311fe9f36de132d1c64668
virtualabishek <717abishekneupane@gmail.com>
2025-01-07 16:22:34 +0545

    learning: completed jan - 7 (day -6 ) day, evening session

--- 20daysCourse/day6/config/connection.js
+++ 20daysCourse/day6/config/connection.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2";
+import "dotenv/config";
 
-connection = mysql.createConnection({
-  host: "localhost",
-  user: "abi",
-  password: "md@12345",
-  database: "exampleDB",
+export default connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-export default connection;
+// export default connection;
