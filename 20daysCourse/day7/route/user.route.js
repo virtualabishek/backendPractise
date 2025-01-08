2355ed644ba66c95ec1e58699aa0af60dfca905f
virtualabishek <717abishekneupane@gmail.com>
2025-01-08 06:21:17 +0545

    learning: completed jan - 8 (day -7 ) morning, first session

--- 20daysCourse/day7/route/user.route.js
+++ 20daysCourse/day7/route/user.route.js
@@ -13,9 +13,75 @@ router.post("/add", async (req, res) => {
       [id, username, location]
     );
     console.log(data);
+    console.log("Data sended.");
     res.status(200).json(data);
   } catch (err) {
     console.log(err);
+  }
+});
+
+//read a data from the db.
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  if (id) {
+    try {
+      const [results] = await connection.query(
+        `SELECT * FROM users WHERE id=?`,
+        [id]
+      );
+      console.log(results);
+      res.json(results);
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ success: false, message: "Database query failed." });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ success: false, message: "User ID cannot be found." });
+  }
+});
+
+// delete a data
+router.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  if (id) {
+    try {
+      connection.query(`DELETE FROM users WHERE id=?`, [id]);
+      res.json({ success: true, message: "User deleted" });
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    res.json({ success: false, message: "User ID cannot find. " });
+  }
+});
+
+// updating the user
+router.put("/update/:id", async (req, res) => {
+  const { id } = req.params;
+  if (id) {
+    try {
+      const { username, location } = req.body;
+      const results = await connection.query(
+        `UPDATE users SET username=?, location=? WHERE id=?`,
+        [username, location, id]
+      );
+      console.log(results);
+      if (results.affectedRows) {
+        res.json({ success: true, message: "User Updated" });
+      } else {
+        res.json({ success: false, message: "Unable to update User" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.json({ success: false, message: "Internal Server Error" });
+    }
+  } else {
+    res.json({ success: false, message: "User Id cannot be found" });
   }
 });
 
