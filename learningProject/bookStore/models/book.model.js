6fba343b8a4f47cb432662a513c1d54ae4426a88
virtualabishek <717abishekneupane@gmail.com>
2025-01-09 07:29:50 +0545

    learning: completed jan - 9 (day -8 ) morning, first session.Revised all the concepts learned before and created a project bookStore and did CRUD operations for books in book model by using seqelize orm..

--- learningProject/bookStore/models/book.model.js
+++ learningProject/bookStore/models/book.model.js
@@ -24,7 +24,7 @@ const bookModel = connection.define(
     },
   },
   {
-    timestamps: true,
+    timestamps: false,
   }
 );
 
--- learningProject/bookStore/routes/book.route.js
+++ learningProject/bookStore/routes/book.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connection from "../models/index.js";
 import bookModel from "../models/book.model.js";
+import { Op } from "sequelize";
 const router = express.Router();
 
 //CRAUD Operation
@@ -19,6 +20,62 @@ router.post("/add", async (req, res) => {
     res
       .status(503)
       .json({ sucess: false, message: "Sorry! Something went wrong." });
+  }
+});
+
+// read a data from a db
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (id) {
+      const data = await bookModel.destroy({
+        where: { id },
+      });
+      console.log(data);
+      if (data) {
+        res.json({ sucess: true, message: "Book is deleted from the db." });
+      } else {
+        res.status(302).json({ sucess: false, message: "Book can't deleted." });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// read a book from database
+router.get("/read/:id", async (req, res) => {
+  const { id } = req.params;
+  if (id) {
+    const data = await bookModel.findByPk(id);
+    res.json(data);
+    console.log(data);
+    if (data) {
+      res.json(data);
+    } else {
+      res.status(503).json({ sucess: false, message: "Book Id is not on db." });
+    }
+  }
+});
+
+// search by the pages
+router.get("/search/by", async (req, res) => {
+  const { totalPages } = req.query;
+  const data = await bookModel.findAll({
+    where: {
+      totalPages: {
+        [Op.like]: `${totalPages}`,
+      },
+    },
+  });
+  console.log(data);
+  if (data) {
+    res.json(data);
+  } else {
+    res.json({
+      success: false,
+      message: "Sorry! Book is not found on the database.",
+    });
   }
 });
 
